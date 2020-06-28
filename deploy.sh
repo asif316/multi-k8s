00fe567a368b89e7e18b6271e1f33d1fd1d12da3
asif316 <36520883+asif316@users.noreply.github.com>
2020-06-27 20:16:00 -0500

    added deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 docker build -t asifhdocker/multi-client:latest -t asifhdocker/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t asifhdocker/multi-server:latest -t asifhdocker/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t asifhdocker/multi-worker:latest -t asifhdocker/multi-worker:$SHA -f ./workerDockerfile ./worker
+docker build -t asifhdocker/multi-worker:latest -t asifhdocker/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push asifhdocker/multi-client:latest
 docker push asifhdocker/multi-server:latest
@@ -12,5 +12,5 @@ docker push asifhdocker/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=asifhdocker/multi-server:$SHA
-kubectl set image deployments/client-deployment server=asifhdocker/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=asifhdocker/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=asifhdocker/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=asifhdocker/multi-worker:$SHA
